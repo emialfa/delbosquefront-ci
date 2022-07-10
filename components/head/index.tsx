77f62f10e095ffb11bfa94724bde23027002cd85
emiliano alfaraz <emialfa2@gmail.com>
2022-07-10 04:10:48 -0300

    wsp

--- components/head/index.tsx
+++ components/head/index.tsx
@@ -9,24 +9,11 @@ interface Props {
 const HeadLayout: React.FC<Props> = ({children}) => {
   return (
     <>
-        <Head>
-            <link
-                href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-                rel="stylesheet" />
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-            <link rel="manifest" href="/site.webmanifest" />
-            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-            <meta name="msapplication-TileColor" content="#da532c" />
-            <meta name="theme-color" content="#ffffff"></meta>
-            <meta property="og:type" content="website" />
-            <meta property="og:url" content="delbosquebordados.com.ar"/>
-            <Script
+        <Script
               strategy="afterInteractive"
               src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
             />
-            <Script
+        <Script
               id="gtag-init"
               strategy="afterInteractive"
               dangerouslySetInnerHTML={{
@@ -40,6 +27,19 @@ const HeadLayout: React.FC<Props> = ({children}) => {
                 `,
               }}
             />
+        <Head>
+            <link
+                href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+                rel="stylesheet" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+            <meta name="msapplication-TileColor" content="#da532c" />
+            <meta name="theme-color" content="#ffffff"></meta>
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="delbosquebordados.com.ar"/>
         </Head>
     {children}
     </>
